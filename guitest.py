7166718685125925f10d10785fb3c2fe5d8c3e7b
nadimakram99 <akramnadim1999@mail.com>
2020-05-10 23:33:18 +0530

    chromeDino

---
+++ guitest.py
@@ -0,0 +1,8 @@
+import pyautogui
+for i in range(0,11):
+    pyautogui.keyDown('f')
+    pyautogui.keyDown('a')
+    pyautogui.keyDown('l')
+    pyautogui.keyDown('a')
+    pyautogui.keyDown('k')
+    pyautogui.keyDown(' ')
